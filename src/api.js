f44829c693eb69b1579ec83647688647996a49b6
stuartfreaks <stuartfreaks@me.com>
2023-01-11 08:41:52 +0100

    Endpoints added

--- src/api.js
+++ src/api.js
@@ -7,6 +7,7 @@ const app = express();
 
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
 
   res.json({
@@ -27,4 +28,64 @@ app.use('/.netlify/functions/api', router);
 
 module.exports.handler = serverless(app);
 
+// get all movies
 
+router.get(
+    '/movies', (req, res) =>
+    res.json({
+        'Movies' : "These Movies"
+    })
+)
+
+// get all Users
+router.get(
+    '/users', (req, res) =>
+    res.json({
+        'Users' : "These Users"
+    })
+)
+
+// get movies by titles
+
+router.get(
+    '/movies/:Title', (req, res) =>
+    res.json({
+        'Movies' : "This Title"
+    })
+)
+
+//get genre by name
+
+router.get(
+    '/movies/genres/:Name', (req, res) =>
+    res.json({
+        'Genres' : "This Genre"
+    })
+)
+
+//get director by name
+
+router.get(
+    '/movies/directors/:Name', (req, res) =>
+    res.json({
+        'Directors' : "Director Name"
+    })
+)
+
+//add user
+
+router.post(
+    '/users', (req, res) =>
+    res.json({
+        'Users' : "Add User"
+    })
+)
+
+// allow users to update their info
+
+router.put(
+    '/users/:Username', (req, res) =>
+    res.json({
+        'Users' : "Update Name"
+    })
+)
